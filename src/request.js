3c7b1a80ceaec0a3b9220cd3dd3ef23c7a0b0ca9
Bobby <bobbykumar706584@gmail.com>
2020-09-17 02:08:27 +0530

    netflix clone

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "01fb2bf2f8fe8757993e5b813540c689";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_generes=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_generes=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_generes=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_generes=10749`,
+  fetchUpcoming: `/movie/upcoming?api_key=${API_KEY}&with_generes=99`,
+};
+
+export default requests;
